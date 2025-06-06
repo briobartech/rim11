fe0c932aec5c076a2a4f55f2b1412c41ea553e22
briobarbtech <barquesi.tech@gmail.com>
2025-06-06 09:23:50 -0300

    styled-component installed

--- rim11/src/App.jsx
+++ rim11/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from 'styled-components'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App () {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <StyledBG>
+      <div className='title'>
+        <h1>RIM11</h1>
+        <div className='banner-box'>
+          <img
+            className='banner'
+            src='src\assets\img\banner.webp'
+            alt=''
+            srcset=''
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </StyledBG>
   )
 }
 
 export default App
+
+const StyledBG = styled.div`
+  .title {
+    background-color: #f5aa45;
+    width: 1080px;
+    height: 100vh;
+    display: flex;
+    align-items: start;
+    flex-direction: column;
+  }
+  .title h1 {
+    font-family: 'Jockey One', sans-serif;
+    font-weight: 800;
+    font-style: normal;
+    font-size: 2em;
+    padding: 20px;
+    color: #04344a;
+    margin: 0 auto;
+  }
+  .banner-box {
+    background: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.5),
+      #f5aa45
+    );
+  }
+  .banner {
+    background-color: #04344a;
+    width: 1080px;
+  }
+`
