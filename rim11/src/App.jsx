f2a5e1a7f03b0287cea294eee28a189e9cfcc4d8
briobarbtech <barquesi.tech@gmail.com>
2025-06-17 20:43:49 -0300

    (feat) navbar component created

--- rim11/src/App.jsx
+++ rim11/src/App.jsx
@@ -1,42 +1,9 @@
-import styled from 'styled-components'
-
+import NavBar from './components/navbar.jsx'
 function App () {
   return (
-    <StyledBG>
-      
-    </StyledBG>
+    <NavBar></NavBar>
+    
   )
 }
 
 export default App
-
-const StyledBG = styled.div`
-  .title {
-    background-color: #f5aa45;
-    width: 1080px;
-    height: 100vh;
-    display: flex;
-    align-items: start;
-    flex-direction: column;
-  }
-  .title h1 {
-    font-family: 'Jockey One', sans-serif;
-    font-weight: 800;
-    font-style: normal;
-    font-size: 2em;
-    padding: 20px;
-    color: #04344a;
-    margin: 0 auto;
-  }
-  .banner-box {
-    background: linear-gradient(
-      to bottom,
-      rgba(0, 0, 0, 0.5),
-      #f5aa45
-    );
-  }
-  .banner {
-    background-color: #04344a;
-    width: 1080px;
-  }
-`
--- rim11/src/components/navbar.jsx
+++ rim11/src/components/navbar.jsx
@@ -1,10 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faCircleInfo, faHouse, faUser } from '@fortawesome/free-solid-svg-icons';
+
 function NavBar () {
   return (
     <div>
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li>   <FontAwesomeIcon icon={faHouse} /></li>
+        <li><FontAwesomeIcon icon={faUser} /></li>
+        <li>Tour</li>
+        <li><FontAwesomeIcon icon={faCircleInfo} /></li>
+        <li><FontAwesomeIcon icon={faBars} /></li>
       </ul>
     </div>
   )
