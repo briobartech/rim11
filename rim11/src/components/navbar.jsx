fa3b243a2d55db3d00e37d207265509d88a07543
briobarbtech <barquesi.tech@gmail.com>
2025-06-17 20:58:54 -0300

    (fix) adddin' flex display to navbar

--- rim11/src/components/navbar.jsx
+++ rim11/src/components/navbar.jsx
@@ -1,9 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faCircleInfo, faHouse, faUser } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components'
 
 function NavBar () {
   return (
-    <div>
+    <StyledNavBar>
+    <div className='navbar-box'>
       <ul>
         <li>   <FontAwesomeIcon icon={faHouse} /></li>
         <li><FontAwesomeIcon icon={faUser} /></li>
@@ -12,7 +14,22 @@ function NavBar () {
         <li><FontAwesomeIcon icon={faBars} /></li>
       </ul>
     </div>
+    </StyledNavBar>
   )
 }
 
 export default NavBar
+const StyledNavBar = styled.div`
+.navbar-box{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 60px;
+    background-color: #333;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+`
