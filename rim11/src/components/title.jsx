5ea1840e5fa21de4d0db0ce4a5dd423e6cd636c4
briobarbtech <barquesi.tech@gmail.com>
2025-06-19 20:58:52 -0300

    (fix) saving title file

--- rim11/src/components/title.jsx
+++ rim11/src/components/title.jsx
@@ -1,17 +1,17 @@
-import {styled} from 'styled-components'
-import { themeData } from '../assets/colors.js';    
+import { styled } from 'styled-components'
+import { themeData } from '../assets/colors.js'
 
-function Title({props}) {
-    return (
-        <StyledTitle>
-            <h1>{props}</h1>
-        </StyledTitle>
-    );
+function Title ({ props }) {
+  return (
+    <StyledTitle>
+      <h1>{props}</h1>
+    </StyledTitle>
+  )
 }
 export default Title
 const StyledTitle = styled.div`
-font-family: 'Jockey One', sans-serif;
-font-size: 4em;
-padding: 20px;
-color: ${themeData.text1};
+  font-family: 'Jockey One', sans-serif;
+  font-size: 4em;
+  padding: 20px;
+  color: ${themeData.text1};
 `
