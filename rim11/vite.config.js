df8b18463cf523868b9ad135757d78af181bf8ce
BrioBartech <barquesi.tech@gmail.com>
2025-07-26 01:18:21 -0300

    sufrimiento AAAAH x2

--- rim11/vite.config.js
+++ rim11/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://qr-historico.space/',
+  base: 'https://www.qr-historico.space/',
   plugins: [react()],
 })
