14e587bf56cf3778864cd28f43a420d862f953f0
BrioBartech <barquesi.tech@gmail.com>
2025-07-26 00:39:12 -0300

    (fix) base - delete /rim11/

--- rim11/vite.config.js
+++ rim11/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'http://qr-historico.space/rim11/',
+  base: 'http://qr-historico.space/',
   plugins: [react()],
 })
